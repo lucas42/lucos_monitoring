b109aa76b268044b6819be43bb468c6239ecd164
Luke Blaney <git@lukeblaney.co.uk>
2019-07-20 02:40:24 +0100

    render list of checks for each service

--- server.erl
+++ server.erl
@@ -69,7 +69,7 @@ getReasonPhrase(StatusCode) ->
 		404 -> "Not Found"
 	end.
 
-stringifyError(Error) ->
+renderError(Error) ->
 	case Error of
 		{http_error, {StatusCode, ReasonPhrase}} ->
 			"Received HTTP response with status "++integer_to_list(StatusCode)++" "++ReasonPhrase;
@@ -81,35 +81,79 @@ stringifyError(Error) ->
 			"An unknown error of type "++atom_to_list(ErrorType)++" occured: "++lists:flatten(io_lib:format("~p",[Error]))
 	end.
 
+renderSystemChecks(SystemChecks) ->
+	{Html, Healthy, CheckCount} = maps:fold(
+		fun (CheckId, CheckInfo, {Html, Healthy, CheckCount}) ->
+			CheckHealthy = maps:get(<<"ok">>, CheckInfo, false),
+			TechDetail = binary_to_list(maps:get(<<"techDetail">>, CheckInfo, <<"-">>)),
+			CheckHtml = "
+				<tr class=\""++getCssClass("check", CheckHealthy)++"\">
+					<td class=\"checkid\">"++binary_to_list(CheckId)++"</td>
+					<td class=\"status\">"++atom_to_list(CheckHealthy)++"</td>
+					<td class=\"techDetail\">"++TechDetail++"</td>
+				</tr>
+			",
+			{Html++CheckHtml, (Healthy and CheckHealthy), CheckCount+1}
+		end, {"", true, 0}, SystemChecks),
+	case CheckCount of
+		0 ->
+			{Healthy, "<span class=\"empty\">No checks found</span>"};
+		_Count ->
+			{Healthy, "
+				<table>
+					<thead><td>Check</td><td>Status</td><td>Details</td></thead>
+					"++Html++"
+				</table>"}
+	end.
+
+getCssClass(Type, Healthy) ->
+	case Healthy of
+		true -> Type ++ " healthy";
+		false -> Type ++ " erroring"
+	end.
+
+renderChecks(Checks) ->
+	maps:fold(
+		fun (Host, Data, Output) ->
+			case Data of
+				{success, System, SystemChecks} ->
+					Name = re:replace(System, "_", " ", [global, {return,list}]),
+					{Healthy, SystemChecksHtml} = renderSystemChecks(SystemChecks),
+					Output++"
+					<div class=\""++getCssClass("system", Healthy)++"\">
+						<h2 class=\"system-name\">"++Name++"</h2>
+						"++SystemChecksHtml++"
+					</div>
+					";
+				{error, Error} ->
+					Output++"
+					<div class=\""++getCssClass("system", false)++"\">
+						<h2>"++Host++"</h2>
+						<p>"++renderError(Error)++"</p>
+					</div>"
+			end
+		end, "", Checks).
+
 controller(_Method, Path, StatePid) ->
 	case Path of
 		"/" ->
 			Checks = gen_server:call(StatePid, {fetch, checks, all}),
 			io:format("Render checks: ~p~n", [Checks]),
-			ChecksOutput = maps:fold(
-				fun (Host, Data, Output) ->
-					case Data of
-						{success, System, SystemChecks} ->
-							Name = re:replace(System, "_", " ", [global, {return,list}]),
-							Output++"
-							<div class=\"system\">
-								<h2 class=\"system-name\">"++Name++"</h2>
-							</div>
-							";
-						{error, Error} ->
-							Output++"
-							<div class=\"system erroring\">
-								<h2>"++Host++"</h2>
-								<p>"++stringifyError(Error)++"</p>
-							</div>"
-					end
-				end, "", Checks),
+			ChecksOutput = renderChecks(Checks),
 			{200, "text/html", "<html><head><title>Lucos Monitoring</title><link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\" /></head><body><h1>Monitoring for Lucos Services</h1>" ++ ChecksOutput ++ "</body></html>"};
 		"/style.css" ->
 			{200, "text/css", "
-			.system h2 { background-color: #060; color: #fff; padding: 0.1em 1em; border-radius: 0.2em; }
+			.system h2 { background-color: #666; color: #fff; padding: 0.1em 1em; border-radius: 0.2em; }
 			h2.system-name { text-transform:capitalize; }
-			.system.erroring h2 { background-color: #900; }
+			.empty { font-style: italic; }
+			table { border-collapse: collapse; }
+			td { border: none thin #ccc; padding: 0.2em 1em; }
+			thead td { font-weight: bold; border-bottom-style: solid; }
+			tr > td:not(:last-child) { border-right-style: solid; }
+			tr:not(:last-child) > td { border-bottom-style: solid; }
+			tr.check td.status { background-color: #666; color: #fff; }
+			.system.healthy h2, tr.check.healthy td.status { background-color: #060; }
+			.system.erroring h2, tr.check.erroring td.status { background-color: #900; }
 			"};
 		_ ->
 			{404, "text/plain", "Not Found"}
